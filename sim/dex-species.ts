02ac1d38529cef308bb5ba109dad86320d301d81
gpansino <gregdawizard@gmail.com>
2023-01-25 18:45:46 -0500

    Update dex-species.ts

--- sim/dex-species.ts
+++ sim/dex-species.ts
@@ -500,7 +500,7 @@ export class DexSpecies {
 				);
 				if (!isLetsGo) species.isNonstandard = 'Past';
 			}
-			if (species.gen == 3) {
+			if (species.gen == 3 && this.dex.gen == 3) {
 				console.log(species.name + " " + species.abilities['0'] + " " + species.abilities['1']);
 			}
 			if (this.dex.currentMod === 'gen8bdsp' &&
