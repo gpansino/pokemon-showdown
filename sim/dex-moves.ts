178c90583756c0c7b88a00b4072f1c1baa5b9e2b
gpansino <gregdawizard@gmail.com>
2023-02-14 00:18:33 -0500

    marowak sprite + adding future moves

--- sim/dex-moves.ts
+++ sim/dex-moves.ts
@@ -575,8 +575,11 @@ export class DataMove extends BasicEffect implements Readonly<BasicEffect & Move
 		}
 
 		if (!this.gen) {
+			if([430, 577, 581, 583, 584, 605].includes(this.num)){
+				this.gen = 3
+			}
 			// special handling for gen8 gmax moves (all of them have num 1000 but they are part of gen8)
-			if (this.num >= 827 && !this.isMax) {
+			else if (this.num >= 827 && !this.isMax) {
 				this.gen = 9;
 			} else if (this.num >= 743) {
 				this.gen = 8;
